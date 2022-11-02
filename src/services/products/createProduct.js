4c780ee63fc94927523efb4ed44163c041aa482f
mohamed-tarek-fouad <mt1348@fayoum.edu.eg>
2022-11-02 22:08:38 +0200

    .

--- src/services/products/createProduct.js
+++ src/services/products/createProduct.js
@@ -9,6 +9,14 @@ export async function createProduct(req, res, next) {
   try {
     const { name, price, catId } = req.body;
     const img = `/uploads/images/${req.file.filename}`;
+    const Product = await prisma.categories.findMany({
+      where: {
+        name,
+      },
+    });
+    if (Product) {
+      return badRequestResponse(res, "this name already exists");
+    }
     const getProduct = await prisma.categories.findFirst({
       where: {
         id: catId,
